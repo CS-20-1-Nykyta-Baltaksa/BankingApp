6386a2d0803572e5428f3134590592e855f0f5a0
Nikita <cloudfreze102@gmail.com>
2023-11-12 17:39:10 +0200

    Init commit, Banking app, without tests

---
+++ TransactionLogger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp
+{
+    public class TransactionLogger
+    {
+        private readonly string _outputFolderPath;
+
+        public TransactionLogger(string outputFolderPath)
+        {
+            _outputFolderPath = outputFolderPath;
+            // Ensure the output folder exists
+        }
+
+        public void LogTransaction(string transactionDetails, bool isSuccess)
+        {
+            string filename = isSuccess ? "SuccessLog.csv" : "FailureLog.csv";
+            string filepath = Path.Combine(_outputFolderPath, filename);
+
+            // Check if file exists and append transaction details
+            // Handle file writing with proper exception handling
+        }
+    }
+
+}
