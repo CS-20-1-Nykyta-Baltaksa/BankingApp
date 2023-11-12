a4d22aa0fcf6951f40c025ec722a66eb4d7e1e4a
Nikita <cloudfreze102@gmail.com>
2023-11-12 17:40:45 +0200

    Tests

---
+++ BankingTest/TransactionLogger.cs
@@ -0,0 +1,18 @@
+﻿using BankingApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingTest
+{
+    public class FakeTransactionLogger : TransactionLogger
+    {
+        public FakeTransactionLogger() : base("dummy_path") { }
+
+        public virtual void LogTransaction(string message, bool isSuccess)
+        {
+        }
+    }
+}
