053b67d52cbb35be90c11569c40d5289c3e59e6c
Nikita <34799980+serpanoid@users.noreply.github.com>
2023-12-26 14:33:56 +0200

    Update Program.cs
    Updated to newest version

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Text.Json;
-using System.Threading;
 using System.Threading.Tasks;
 using BankingApp;
 
@@ -24,44 +23,26 @@ class Program
 
         Directory.CreateDirectory(outputFolderPath);
 
-        int successfulTransactionCount = 0;
-        int unsuccessfulTransactionCount = 0;
-
-        string[] inputFileNames = Directory.GetFiles(inputFolderPath, "*.csv");
+        var inputFileNames = Directory.GetFiles(inputFolderPath, "*.csv");
         Array.Sort(inputFileNames);
 
-        var tasks = new List<Task>();
-        var threads = new List<Thread>();
+        var tasks = new List<Task<TaskResult>>();
 
         foreach (var fileName in inputFileNames)
         {
-            tasks.Add(Task.Run(async () =>
-            {
-                await ProcessFileAsync(fileName, debitCard, ref successfulTransactionCount, ref unsuccessfulTransactionCount);
-            }));
-
-            Thread thread = new Thread(() =>
-            {
-                ProcessFile(fileName, debitCard, ref successfulTransactionCount, ref unsuccessfulTransactionCount);
-            });
-            threads.Add(thread);
-
-            ThreadPool.QueueUserWorkItem((state) =>
-            {
-                ProcessFile(fileName, debitCard, ref successfulTransactionCount, ref unsuccessfulTransactionCount);
-            });
+            tasks.Add(ProcessFileAsync(fileName, debitCard));
         }
 
         await Task.WhenAll(tasks);
 
-        foreach (var thread in threads)
-        {
-            thread.Start();
-        }
+        int successfulTransactionCount = 0;
+        int unsuccessfulTransactionCount = 0;
 
-        foreach (var thread in threads)
+        foreach (var task in tasks)
         {
-            thread.Join();
+            var result = await task;
+            successfulTransactionCount += result.SuccessfulCount;
+            unsuccessfulTransactionCount += result.UnsuccessfulCount;
         }
 
         var resultData = new
@@ -78,9 +59,11 @@ class Program
         Console.WriteLine("Processing completed. Results are stored in the output folder.");
     }
 
-    static async Task ProcessFileAsync(string fileName, DebitCard debitCard, ref int successfulTransactionCount, ref int unsuccessfulTransactionCount)
+    static async Task<TaskResult> ProcessFileAsync(string fileName, DebitCard debitCard)
     {
         var lines = await File.ReadAllLinesAsync(fileName);
+        int successfulTransactionCount = 0;
+        int unsuccessfulTransactionCount = 0;
 
         foreach (var line in lines)
         {
@@ -103,18 +86,18 @@ class Program
             {
                 case "Deposit":
                     debitCard.Deposit(amount);
-                    Interlocked.Increment(ref successfulTransactionCount);
+                    successfulTransactionCount++;
                     break;
 
                 case "Withdrawal":
                     bool isSuccess = debitCard.Withdraw(amount);
                     if (isSuccess)
                     {
-                        Interlocked.Increment(ref successfulTransactionCount);
+                        successfulTransactionCount++;
                     }
                     else
                     {
-                        Interlocked.Increment(ref unsuccessfulTransactionCount);
+                        unsuccessfulTransactionCount++;
                     }
                     break;
 
@@ -123,52 +106,19 @@ class Program
                     break;
             }
         }
+
+        return new TaskResult(successfulTransactionCount, unsuccessfulTransactionCount);
     }
+}
 
-    static void ProcessFile(string fileName, DebitCard debitCard, ref int successfulTransactionCount, ref int unsuccessfulTransactionCount)
+class TaskResult
+{
+    public int SuccessfulCount { get; }
+    public int UnsuccessfulCount { get; }
+
+    public TaskResult(int successfulCount, int unsuccessfulCount)
     {
-        var lines = File.ReadAllLines(fileName);
-
-        foreach (var line in lines)
-        {
-            var parts = line.Split(',');
-
-            if (parts.Length != 2)
-            {
-                Console.WriteLine($"Invalid line format: {line}");
-                continue;
-            }
-
-            string transactionType = parts[0].Trim();
-            if (!decimal.TryParse(parts[1].Trim(), out decimal amount) || amount <= 0)
-            {
-                Console.WriteLine($"Invalid transaction amount: {parts[1]}");
-                continue;
-            }
-
-            switch (transactionType)
-            {
-                case "Deposit":
-                    debitCard.Deposit(amount);
-                    Interlocked.Increment(ref successfulTransactionCount);
-                    break;
-
-                case "Withdrawal":
-                    bool isSuccess = debitCard.Withdraw(amount);
-                    if (isSuccess)
-                    {
-                        Interlocked.Increment(ref successfulTransactionCount);
-                    }
-                    else
-                    {
-                        Interlocked.Increment(ref unsuccessfulTransactionCount);
-                    }
-                    break;
-
-                default:
-                    Console.WriteLine($"Unknown transaction type: {transactionType}");
-                    break;
-            }
-        }
+        SuccessfulCount = successfulCount;
+        UnsuccessfulCount = unsuccessfulCount;
     }
 }
